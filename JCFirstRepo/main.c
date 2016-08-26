9018b33a0df6b8656c107898bae009abfb5bc677
Student P_02 <studentp_02@Student-P-02.local>
2016-08-26 13:45:16 +0530

    Initial Commit

---
+++ JCFirstRepo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  JCFirstRepo
+//
+//  Created by Student P_02 on 26/08/16.
+//  Copyright © 2016 Jivan Chaudhari. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
